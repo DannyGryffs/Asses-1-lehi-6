f71ee61f7db886b0176c7be1c41f55d83d82c1b2
Daniel Griffin <dgriffinpv@gmail.com>
2023-12-01 16:22:48 -0700

    EOD-Fri

--- src/arrays.js
+++ src/arrays.js
@@ -2,32 +2,75 @@
 // Ex.:
 //   greaterThanTen([1, 2, 3, 11, 12, 13]);
 //   => [11, 12, 13]
-function greaterThanTen(numbers) {}
+//complete
+function greaterThanTen(numbers) {
+  let newArr = []
+  for(let i = 0; i < numbers.length; i++){
+  if(numbers[i] > 10){
+    newArr.push(numbers[i])
+    }
+  }
+  return newArr
+}
+greaterThanTen([1, 2, 3, 11, 12, 13]);
 
 // Given an array of strings, return all words that start with 'b' or 'B'.
 // Ex.:
 //   bWords(['banana', 'orange', 'apple', 'Bonobo', 'kiwi', 'pear']);
 //   => ['banana', 'Bonobo]
-function bWords(words) {}
-
+//complete
+function bWords(words) {
+  let newArr = []
+  for(let i = 0; i < words.length; i++)
+  if(words[i].includes('b') || words[i].includes('B')){
+    newArr.push(words[i])
+  }
+  return newArr
+}
+bWords(['banana', 'orange', 'apple', 'Bonobo', 'kiwi', 'pear']);
 // Add all the elements from additionalItems to the end of originalArray.
 // Return the originalArray..
 // Ex.:
 //  extend([1, 2, 3], [4, 5, 6]);
 //  => [1, 2, 3, 4, 5, 6]
-function extend(originalArray, additionalItems) {}
+//complete
+function extend(originalArray, additionalItems) {
+  
+  originalArray.push(additionalItems)
+  //use spread operator
+  return originalArray
+}
+extend([1, 2, 3], [4, 5, 6]);
 
 // Return an array of all items with the given length.
 // Ex.:
 //   itemsWithLength(['a', 'bbb', 'cccc', 'dddddd', 'eee'], 3);
 //   => ['bbb', 'eee']
-function itemsWithLength(items, length) {}
+//complete
+function itemsWithLength(items, length) {
+  let newArr = [];
+  for(let i = 0; i < items.length; i++) {
+    if(items[i].length === length){
+      newArr.push (items[i]);
+    }
+  }
+  return newArr;
+}
+itemsWithLength(['a', 'bbb', 'cccc', 'dddddd', 'eee'], 3);
 
 // Return an array with every other element from the input array (start with index 0).
 // Ex.:
 //   everyOtherItem(['a', 'b', 'c', 'd', 'e']);
 //   => ['a', 'c', 'e']
-function everyOtherItem(items) {}
+//complete
+function everyOtherItem(items) {
+  let newArr = [];
+  for(let i = 0; i < items.length; i += 2){
+    newArr.push(items[i]);
+  }
+  return newArr;
+}
+everyOtherItem(['a', 'b', 'c', 'd', 'e']);
 
 // Given a list of words and a letter, return the indexes of the words that
 // start with that letter. You can assume that the words and letter will always
@@ -35,7 +78,16 @@ function everyOtherItem(items) {}
 // Ex.:
 //   findWordsStartingWith(['apple', 'banana', 'kiwi', 'pear', 'bacon'], 'b');
 //   => [1, 4]
-function findWordsStartingWith(words, letter) {}
+//TBC
+function findWordsStartingWith(words, letter) {
+  const newArr = [];
+  for(let i = 0; i < words.length; i++){
+    if(words[i].length.startsWith(letter)){
+      newArr.push(words[i])
+    }
+  }
+
+}
 
 // Return the `n` smallest values in the array in descending order (largest
 // numbers first). Assume that `n` will always be less than the length of the
@@ -58,7 +110,11 @@ function findIndex(items, value) {}
 // Ex.:
 //   range(1, 5);
 //   => [1, 2, 3, 4, 5]
-function range(start, stop) {}
+function range(start, stop) {
+  //you need to write a conditional statement
+}
+range(1, 5);
+
 
 export {
   bWords,
--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,10 @@
 // Ex.:
 //   divide(10, 2);
 //   => 5
-function divide(x, y) {}
+function divide(x, y) {
+return  x / y;
+}
+divide(10, 2);
 
 // Return the average of x and y.
 // Here, the average of x and y is the *mean* of x and y. In other words, it's
@@ -10,27 +13,43 @@ function divide(x, y) {}
 // Ex.:
 //   average(10, 2);
 //   => 6
-function average(x, y) {}
-
+function average(x, y) {
+  return (x + y) / 2;
+}
+average(10, 2)
 // Return true if x and y are mostly equivalent up to a tolerance of 0.001.
 // In other words, return true if the aboslute value of x - y is less than 0.001.
 // Ex.:
 //   approximatelyEqual(10.001, 10);
 //   => true
-function approximatelyEqual(x, y) {}
+function approximatelyEqual(x, y) {
+  if(x - y < 0.001){
+    return true
+  }else{
+    return false;
+  }
+}
+approximatelyEqual(10.001, 10);
 
 // Given a first name and last name, return a full name in the format "FIRST LAST"
 // Ex.:
 //   fullName('John', 'Doe');
 //   => 'John Doe'
-function fullName(firstName, lastName) {}
-
+function fullName(firstName, lastName) {
+  return firstName + ' ' + lastName
+}
+fullName('John', 'Doe');
 // Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
 // person, beverage and location provided.
 // Ex.:
 //   generateSentence('Kay', 'coffee', 'the local cafe');
 //   => 'Kay was drinking coffee at the local cafe.'
-function generateSentence(person, beverage, location) {}
+function generateSentence(person, beverage, location) {
+  const madLib = `${person} was drinking ${beverage} at the ${location}`;
+  return madLib;
+  }
+  generateSentence('Kay', 'coffee', 'the local cafe');
+  
 
 // Return the given string with all vowels replced with '*' characters.
 // Ex.:
